6a24bdaaee6b9600020674ccc0851c1e40c7381a
faheem <faheemwork23@gmail.com>
2023-09-17 06:11:36 +0530

    test /execute

--- test.js
+++ test.js
@@ -1,6 +1,13 @@
 function sum(a, b) {
-  return a * b;
+  return a + b;
 }
 
 console.log(sum(2, 3));
 console.log("hello world");
+
+function product(a, b) {
+  return a * b;
+}
+
+console.log(product(3, 4));
+console.log("end line");
